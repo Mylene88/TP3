7cde93c58cc1cd22b4573c7e5a18baad33a3524a
Havarell <havarellyo68@gmail.com>
2022-03-16 02:12:40 +0100

    ajout du hostname

--- client2.py
+++ client2.py
@@ -23,14 +23,15 @@ class MainWindow(QWidget):
         
         self.label1 = QLabel("Enter your IP:", self)
         self.label3 = QLabel("Enter your hostname:", self)
-        self.text.move(10, 30)
         self.text = QLineEdit(self)
         self.text.move(10, 30)
         self.label2 = QLabel("Answer:", self)
         self.label2.move(10, 60)
+        self.text = QLineEdit(self)
+        self.text.move(10, 120)
         self.label3.move(10, 100)
         self.label3 = QLabel("Answer:", self)
-        self.label3.move(10, 120)
+        self.label3.move(10, 150)
         self.button = QPushButton("Send", self)
         self.button.move(10, 300)
     
